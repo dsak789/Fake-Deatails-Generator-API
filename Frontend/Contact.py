149c8f336c98bb3aba478fbcc0ba1e0515f07130
dsak789 <dsak.official@gmail.com>
2024-08-29 23:02:48 +0530

    UI

--- Frontend/Contact.py
+++ Frontend/Contact.py
@@ -6,3 +6,4 @@ class Contact:
         st.write("For any queries or support, contact us at:")
         st.write("- Email: dsak.official@gmail.com")
         st.write("- GitHub: [Faker789 GitHub Repository](https://github.com/dsak789/Profile-Generator-API)")
+        st.write("- GitHub To Fork: [Faker789 GitHub Repository](https://github.com/dsak789/Profile-Generator-API/fork)")
